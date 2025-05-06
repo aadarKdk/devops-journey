eb6341195f841b27d672a48893cf5979deb8b295
erkdk <aadar.kdk@gmail.com>
2025-05-06 12:22:37 +0000

    4-variable.sh script added

---
+++ 02-shell-scripting/scripts/4-variable.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Some variables are environment variables.
+
+NAME='erkdk' # this variable is also kept in ~/.bashrc file in my case.
+
+echo "Date: "
+date			# date command
+echo
+echo " Host-name: " $HOSTNAME
+echo " User-name: " $USER
+echo " My GitHub account name is $NAME" # displays exported variable from ~/.bashrc file.
+echo " Line No. " $LINENO
+echo " Your lucky no. may be " $RANDOM
+sleep 2s
+echo " The Process ID of current running shell: " $$
+echo " Time taken: $SECONDS"
+echo "Exit code of Last command: $?"
+
